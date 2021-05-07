99902548c81a2f9847cae147f28f628422b8e7fc
Zentroz <anibaltoro.a@gmail.com>
2021-05-06 22:40:22 -0400

    se corrige model

--- Frontend/tienda-ark/src/app/componets/dinos/dinos.component.ts
+++ Frontend/tienda-ark/src/app/componets/dinos/dinos.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MenuService } from '../../services/menu.service';
 import { DinoService} from '../../services/dinos.service';
-import { Dino } from 'src/app/models/Dino';
+import { Dino } from '../../models/Dino';
 
 @Component({
   selector: 'app-dinos',
