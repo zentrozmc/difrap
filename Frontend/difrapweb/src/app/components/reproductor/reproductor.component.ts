733f20da3e750749929cd51cef186e56bd1d9c08
Anibal <djconkers@gmail.com>
2021-06-20 23:29:15 -0400

    se actualiza reproductor

--- Frontend/difrapweb/src/app/components/reproductor/reproductor.component.ts
+++ Frontend/difrapweb/src/app/components/reproductor/reproductor.component.ts
@@ -1,5 +1,6 @@
 import { analyzeAndValidateNgModules } from '@angular/compiler';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Track } from 'src/app/models/track';
 import { AudioService } from 'src/app/services/audio.service';
 
 @Component({
@@ -10,19 +11,19 @@ import { AudioService } from 'src/app/services/audio.service';
 })
 export class ReproductorComponent implements OnInit {
 
-  public id:String=null;
-
+  @Input() public listaTrack:Array<Track>=[];
   public duracion:number=0;
   public duracionTotal:number=0;
+  public carga:number;
   public tiempo;
   public tiempoTotal;
+  public estado="loading";
   public audio:HTMLAudioElement;
   constructor(
 	  private _audioService:AudioService
   ) { }
 
   ngOnInit(): void {
-    this.cargarReproductor();
 	this._audioService.getTimeRemaining().subscribe(
 		result=>{
 			//this.tiempo=result;
@@ -30,17 +31,17 @@ export class ReproductorComponent implements OnInit {
 	);
 	this._audioService.getPlayerStatus().subscribe(
 		result=>{
-			console.log("getPlayerStatus",result);
+      this.estado = result;
 		}
 	);
 	this._audioService.getPercentElapsed().subscribe(
 		result=>{
-			console.log("getPercentElapsed",result);
+      this.duracion = result;
 		}
 	);
 	this._audioService.getPercentLoaded().subscribe(
 		result=>{
-			console.log("getPercentLoaded",result);
+      this.carga=result;
 		}
 	);
 	this._audioService.getTimeElapsed().subscribe(
@@ -52,66 +53,23 @@ export class ReproductorComponent implements OnInit {
 
   play(id)
   {
-	  
-	this.audio = document.getElementById("reproductor") as HTMLAudioElement;
-	this.audio.play();
-	this.audio.addEventListener("timeupdate",this.mover);
-	this.duracionTotal = this.audio.duration;
-	this._audioService.toggleAudio();
-	
+    if(this._audioService.getAudio().src)
+	    this._audioService.toggleAudio();
+    else
+      this.cambiarTrack(0);
   }
 
   posicionar()
   {
-	this.audio.currentTime = this.duracion;
-	console.log("duracion",this.duracion);
+	  this.audio.currentTime = this.duracion;
+	  console.log("duracion",this.duracion);
   }
 
-  
-  private mover =(evt) =>
-  {
-	this.duracion=this.audio.currentTime;
-	console.log("audio.currentTime",this.audio.currentTime);
-	console.log("audio.duration",this.audio.duration);
-	console.log("audio.ended",this.audio.ended);
-	console.log("audio.muted",this.audio.muted);
+  cambiarTrack(track) 
+  { 
+    if(track>-1)
+    {
+      this._audioService.setAudio("http://difrap.cl:8080/apiDifRap/track/play/"+this.listaTrack[track].drive);
+    }
   }
-  
-
-
-   cambiarTrack(track) 
-   {
-    let viejo_audio = document.getElementById("reproductor")
-    let audio_padre = viejo_audio.parentNode
-    audio_padre.removeChild(viejo_audio)
-    let nuevo_audio = document.createElement("audio")
-    nuevo_audio.setAttribute("id","reproductor")
-    nuevo_audio.setAttribute("controls", "controls")
-    nuevo_audio.setAttribute("autoplay", "autoplay")
-    nuevo_audio.setAttribute("preload", "auto")
-    let source = document.createElement("source")
-    source.setAttribute("src", "http://difrap.cl:8080/apiDifRap/track/play/"+track)
-    source.setAttribute("type", "audio/mpeg")
-    source.setAttribute("id", "reproductorSource")
-    nuevo_audio.appendChild(source)
-    audio_padre.appendChild(nuevo_audio)
-
-
-	//this._audioService.setAudio("http://difrap.cl:8080/apiDifRap/track/play/"+track);
-  }
-
-  cargarReproductor() 
-  {
-    let nuevo_audio = document.createElement("audio")
-    nuevo_audio.setAttribute("id","reproductor")
-    nuevo_audio.setAttribute("controls", "controls")     
-	nuevo_audio.setAttribute("autoplay", "autoplay")  
-    let source = document.createElement("source")
-    source.setAttribute("src", "")
-    source.setAttribute("type", "audio/mpeg")
-    source.setAttribute("id", "reproductorSource")
-    nuevo_audio.appendChild(source)
-    let padre = document.getElementById("reproductorBox")
-    padre.appendChild(nuevo_audio)
- }
 }
